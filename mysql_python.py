b632ed2857e17ee5858471e712347f3bdb584232
Andrew Porritt <andrew.porritt@icloud.com>
2019-03-30 22:17:11 +0000

    added templates folder, base, landin and login pages HTML boilerplate

--- mysql_python.py
+++ mysql_python.py
@@ -1,20 +1,36 @@
 import os
 import pymysql
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
 
 app = Flask (__name__)
 
 @app.route('/')
-def hello():
-    return "hello world - innit"
-    
-@app.route('/test')
-def testSuccess():
-    return "test is successful Brah!"
+def getLanding():
+    return render_template("landing_page.html")
 
+@app.route('/login')
+def getLogin():
+    return render_template("login.html")
     
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
         port=int(os.environ.get('PORT')),
         debug=True)
 
+username = os.getenv('C9_USER')
+
+connection = pymysql.connect(host='localhost',
+                            user=username,
+                            password='',
+                            db='RecipeBook')
+                            
+try:
+    with connection.cursor() as cursor:
+        sql = "SELECT user_name FROM USER;"
+        cursor.execute(sql)
+        result=cursor.fetchall()
+        print(result)
+
+finally:
+    connection.close()
+    
