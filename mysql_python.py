b34221115d99049b69c6cd4bf299f29145f1c09d
Andrew Porritt <andrew.porritt@icloud.com>
2019-03-30 21:40:48 +0000

    additional test page added and heroku deployment tested

--- mysql_python.py
+++ mysql_python.py
@@ -8,25 +8,13 @@ app = Flask (__name__)
 def hello():
     return "hello world - innit"
     
+@app.route('/test')
+def testSuccess():
+    return "test is successful Brah!"
+
+    
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
         port=int(os.environ.get('PORT')),
         debug=True)
 
-username = os.getenv('C9_USER')
-
-connection = pymysql.connect(host='localhost',
-                            user=username,
-                            password='',
-                            db='RecipeBook')
-                            
-try:
-    with connection.cursor() as cursor:
-        sql = "SELECT user_name FROM USER;"
-        cursor.execute(sql)
-        result=cursor.fetchall()
-        print(result)
-
-finally:
-    connection.close()
-    
