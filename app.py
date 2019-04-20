ae052c497c3b0de6a7ea4e62af110986fbaba9d8
Andrew Porritt <andrew.porritt@icloud.com>
2019-04-20 12:55:37 +0000

    switched from mySQL to MongoDB. Connection successful and db showing in flask templates

---
+++ app.py
@@ -0,0 +1,24 @@
+import os
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
+app = Flask (__name__)
+
+app.config["MONGO_DBNAME"] = "RecipeBook"
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
+
+mongo = PyMongo(app)
+
+@app.route('/')
+@app.route('/recipes')
+def get_recipes():
+    return render_template("recipes.html", result=mongo.db.recipe.find())
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+        port=int(os.environ.get('PORT')),
+        debug=True)
+
+
+                            
